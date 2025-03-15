b8bb95be90d280f2f002d71d51796122a275a0c5
IldarAkhm <112767933+IldarAkhm@users.noreply.github.com>
2025-03-15 13:57:40 +0300

    berttrain

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,4 +1,6 @@
-from Bot.TOKEN import TOKEN
+import torch
+
+from TOKEN import TOKEN
 from aiogram import executor, Bot, Dispatcher, types
 from get_model import Model
 from TextPreprocess.BertTextPreproccess import BertTextPreprocess
@@ -13,13 +15,12 @@ preprocessor = BertTextPreprocess(tokenizer)
 async def text_cls(message: types.Message):
     text = message.text
     pr_output = preprocessor.preprocess(text)
-    input_ids = pr_output['input_ids'].to('cpu')
-    attention_mask = pr_output['attention_mask'].to('cpu')
+    input_ids = pr_output['input_ids'].unsqueeze(0).to('cpu')
+    attention_mask = pr_output['attention_mask'].unsqueeze(0).to('cpu')
     output = model(input_ids=input_ids, attention_mask=attention_mask)
-    print(output)
-    # pred = torch.argmax(output.logits, dim=1).cpu().numpy()
-
-    # await Bot.send_message(message.from_user.id, str(pred))
+    pred = torch.argmax(output.logits, dim=1).cpu().numpy()
+    await Bot.send_message(message.from_user.id, str(output.logits))
+    await Bot.send_message(message.from_user.id, str(pred))
 
 
 if __name__ == '__main__':
